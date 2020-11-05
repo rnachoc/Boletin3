cc20c6fba746096a3231828a686e1cdf4a744407
rnachoc <xxrnachocxx@gmail.com>
2020-11-05 12:17:10 +0100

    Actualizacion clase Principal.java

---
+++ B3_P1/src/Principal.java
@@ -0,0 +1,26 @@
+public class Principal {
+
+    public class Controll{
+
+
+
+
+    }
+
+    public class ThreadPasajero implements Runnable{
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+    public class ThreadCoche implements Runnable{
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+}
