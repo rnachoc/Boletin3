51437fab479ed37fb6e183241ae0ce80e60549cb
rnachoc <xxrnachocxx@gmail.com>
2020-11-10 08:56:59 +0100

    Actualizacion clase Principal.java

--- B3_P1/src/Principal.java
+++ B3_P1/src/Principal.java
@@ -5,9 +5,10 @@ import java.util.concurrent.Semaphore;
 
 public class Principal {
 
-    public class Controll{
+    public class Controll {
 
         public int iCoches = 3;
+
         public Semaphore coches = new Semaphore(iCoches);
         public Semaphore pasajeros = new Semaphore(0);
 
@@ -16,12 +17,13 @@ public class Principal {
 
     Controll controll = new Controll();
 
-    public class ThreadPasajero implements Runnable{
-        byte id = 0;
+    public class ThreadPasajero implements Runnable {
+        private byte id = 0;
 
-        public ThreadPasajero(byte id){
+        public ThreadPasajero(byte id) {
             this.id = id;
         }
+
         @Override
         public void run() {
             byte iTiempoRandom = (byte) (Math.random() * 10);
@@ -37,68 +39,68 @@ public class Principal {
 
             controll.colaThreadPasajero.add(this);
             controll.pasajeros.release();
+        }
     }
 
-    public class ThreadCoche implements Runnable{
-        byte id = 0;
+        public class ThreadCoche implements Runnable {
+            private byte id = 0;
 
-        public ThreadCoche(byte id) {
-            this.id = id;
-        }
-
-        @Override
-        public void run() {
-
-            while (true){
-
-                System.out.println("Coche " + id + " esta libre.");
-                try {
-                    controll.coches.acquire();
-                    controll.pasajeros.acquire();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                int iPasajeroAcabado = controll.colaThreadPasajero.poll().id;
-
-                System.out.println("El pasajero: " + iPasajeroAcabado + " se ha montado en el coche: " + id);
-                System.out.println("El coche: " + id + " .esta en circulacion");
-
-                controll.cochesRestantes.release();
-
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                System.out.println("El coche: " + id + " ha regresado.");
-                controll.pasajerosRestantes.release();
+            public ThreadCoche(byte id) {
+                this.id = id;
             }
 
+            @Override
+            public void run() {
+
+                while (true) {
+
+                    System.out.println("Coche " + id + " esta libre.");
+                    try {
+                        controll.coches.acquire();
+                        controll.pasajeros.acquire();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    int iPasajeroAcabado = controll.colaThreadPasajero.poll().id;
+
+                    System.out.println("El pasajero: " + iPasajeroAcabado + " se ha montado en el coche: " + id);
+                    System.out.println("El coche: " + id + " esta en circulacion");
+
+                    try {
+                        // TIEMPO DE EJECUCION DE LA ATRACCION
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.out.println("El coche: " + id + " ha regresado.");
+                    System.out.println("El pasajero: " + iPasajeroAcabado + " se marcha.");
+
+                    controll.coches.release();
+                }
+            }
         }
-    }
 
-    private void executeMultiThreading() throws InterruptedException{
-        int a = 0;
-        for(int i = 0; i < controll.iCoches; i++) {
-            new Thread(new ThreadCoche((byte) i)).start();
+        private void executeMultiThreading() throws InterruptedException {
+            int a = 0;
+            for (int i = 0; i < controll.iCoches; i++) {
+                new Thread(new ThreadCoche((byte) i)).start();
+            }
+            while (true) {
+                Thread.sleep(500);
+                new Thread(new ThreadPasajero((byte) a)).start();
+                a++;
+            }
         }
-        while (true){
-            new Thread(new ThreadPasajero((byte) a)).start();
-            a++;
-            Thread.sleep(500);
 
+        public static void main(String[] args) {
+
+            Principal principal = new Principal();
+            try {
+                principal.executeMultiThreading();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-
-        Principal principal = new Principal();
-        principal.executeMultiThreading();
-
-
-    }
-
 }
