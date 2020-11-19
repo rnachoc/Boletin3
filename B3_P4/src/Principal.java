743f0d089dfbb69f7a87f13a228b22482bcff375
rnachoc <xxrnachocxx@gmail.com>
2020-11-19 18:44:10 +0100

    Actualizacion clase Principal.java problema 4

--- B3_P4/src/Principal.java
+++ B3_P4/src/Principal.java
@@ -1,4 +1,22 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.Semaphore;
+
 public class Principal {
+    public class Controll {
+        public Semaphore semaphore = new Semaphore(1);
+
+        public Queue<subir> colaThreadComensal = new LinkedList<subir>();
+        public Queue<bajar> colaThreadComensal = new LinkedList<bajar>();
+
+
+
+
+
+
+
+
+
 
 
 }
