cfe7670669cca0e7cead1427e5dfc8728d2db299
rnachoc <xxrnachocxx@gmail.com>
2020-11-19 18:45:00 +0100

    Actualizacion clase Principal.java problema 4

--- B3_P4/src/Principal.java
+++ B3_P4/src/Principal.java
@@ -9,11 +9,59 @@ public class Principal {
         public Queue<subir> colaThreadComensal = new LinkedList<subir>();
         public Queue<bajar> colaThreadComensal = new LinkedList<bajar>();
 
+    }
 
+    Controll controll = new Controll();
 
+    public class subir implements Runnable {
+        private byte id = 0;
 
+        public subir(byte id) {
+            this.id = id;
+        }
 
+        @Override
+        public void run() {
+            byte iTiempoRandom = (byte) (Math.random() * 10);
+            System.out.println("El comensal: " + id + " llega a la cola en " + iTiempoRandom + " segundos.");
 
+            try {
+                Thread.sleep(iTiempoRandom * 1000);
+
+            } catch (InterruptedException e) {
+                System.out.println("ERROR! ");
+                e.printStackTrace();
+            }
+
+            controll.colaThreadComensal.add(this);
+            controll.comensales.release();
+        }
+    }
+
+    public class bajar implements Runnable {
+        private byte id = 0;
+
+        public ThreadComensales(byte id) {
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            byte iTiempoRandom = (byte) (Math.random() * 10);
+            System.out.println("El comensal: " + id + " llega a la cola en " + iTiempoRandom + " segundos.");
+
+            try {
+                Thread.sleep(iTiempoRandom * 1000);
+
+            } catch (InterruptedException e) {
+                System.out.println("ERROR! ");
+                e.printStackTrace();
+            }
+
+            controll.colaThreadComensal.add(this);
+            controll.comensales.release();
+        }
+    }
 
 
 
