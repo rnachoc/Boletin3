7e461b266fd333852357a6237e7172dc232da8dc
rnachoc <xxrnachocxx@gmail.com>
2020-11-19 18:38:53 +0100

    Creacion problema 4

---
+++ B3_P3/src/Principal.java
@@ -0,0 +1,62 @@
+import java.util.concurrent.Semaphore;
+
+public class Principal {
+
+    public class Controll{
+
+        private Semaphore Tabaco = new Semaphore(0);
+        private Semaphore Papel = new Semaphore(0);
+        private Semaphore Cerilla = new Semaphore(0);
+
+
+
+
+
+    }
+
+    public synchronized void run(){
+        setiCerilla
+
+
+
+
+    }
+
+
+    public class Malboro implements Runnable{
+
+
+        @Override
+        public synchronized void run() {
+            String sResultado = "";
+            do{
+
+
+
+
+
+            }
+
+
+
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- E6_B3/src/Principal.java
+++ E6_B3/src/Principal.java
@@ -1,2 +1,17 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.Semaphore;
+
 public class Principal {
+
+    public class Controll{
+
+        private Semaphore S1 = new Semaphore(0);
+        private Semaphore
+
+        private Queue<Thread> cola = new LinkedList<Thread>();
+
+    }
+
+
 }
