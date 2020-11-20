3617109e1f6296b1cd804dc8dd9d5532008060de
rnachoc <xxrnachocxx@gmail.com>
2020-11-20 10:15:11 +0100

    Actualizacion clase Principal.java problema 4

--- B3_P4/src/Principal.java
+++ B3_P4/src/Principal.java
@@ -15,11 +15,6 @@ public class Principal {
         public Queue<Alumno> colaSubir = new LinkedList<Alumno>();
         public Queue<Alumno> colaBajar = new LinkedList<Alumno>();
 
-        private boolean bSubir = false;
-        private boolean bBajar = false;
-        private byte alumnosbajando = 0;
-        private byte alumnossubiendo = 0;
-
         public Semaphore getSemaphoreSubir() {
             return semaphoreSubir;
         }
@@ -51,105 +46,57 @@ public class Principal {
         public void setColaBajar(Queue<Alumno> colaBajar) {
             this.colaBajar = colaBajar;
         }
-
-        public boolean isbSubir() {
-            return bSubir;
-        }
-
-        public void setbSubir(boolean bSubir) {
-            this.bSubir = bSubir;
-        }
-
-        public boolean isbBajar() {
-            return bBajar;
-        }
-
-        public void setbBajar(boolean bBajar) {
-            this.bBajar = bBajar;
-        }
-
-        public byte getAlumnosbajando() {
-            return alumnosbajando;
-        }
-
-        public void setAlumnosbajando(byte alumnosbajando) {
-            this.alumnosbajando = alumnosbajando;
-        }
-
-        public byte getAlumnossubiendo() {
-            return alumnossubiendo;
-        }
-
-        public void setAlumnossubiendo(byte alumnossubiendo) {
-            this.alumnossubiendo = alumnossubiendo;
-        }
     }
 
     private final Controll controll = new Controll();
 
     public class Escalera implements Runnable {
-        private byte id = 0;
-
-        public Esalera(byte id) {
-            this.id = id;
-        }
-
-        public byte getId() {
-            return id;
-        }
-
-        public void setId(byte id) {
-            this.id = id;
-        }
-
         @Override
         public void run() {
-            try {
-                controll.semaphoreBajar.acquire();
-                controll.colaSubir.add(this);
-                controll.bSubir = true;
-                if(controll.colaBajar.isEmpty() && controll.alumnossubiendo != 10 || controll.bSubir){
-                    controll.bBajar = false;
+           do {
 
-                    System.out.println("La cola para subir: " + getId() + " va a subir.");
-                    controll.alumnosbajando++;
-                    Thread.sleep(2000);
+               try {
+                   Thread.sleep(400);
 
-                    System.out.println("El alumno para subir: " + getId() + " ha subido");
-                    controll.semaphoreBajar.release();
-                }
+                   if(controll.semaphoreSubir.getQueueLength() != 0 && controll.semaphoreBajar.getQueueLength() != 0){
 
-                if (controll.alumnossubiendo == 10){
-                    controll.bSubir = false;
-                    System.out.println("Han subido 10 alumnos.");
-                    for (int i = 0; i < MAX_ESTUDIANTES ; i++){
-                        controll.colaBajar.poll();
+                       if(controll.semaphoreBajar.getQueueLength() >= MAX_ESTUDIANTES || controll.semaphoreSubir.getQueueLength() == 0){
+                            byte bColaBajada = (byte) controll.semaphoreBajar.getQueueLength();
+                            if(controll.semaphoreBajar.getQueueLength() > MAX_ESTUDIANTES){
+                                bColaBajada = MAX_ESTUDIANTES;
+                            }
+                           System.out.println("Han bajado: " + bColaBajada + " estudiantes.");
+                            controll.semaphoreBajar.release();
 
-
-                    }
-                }
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-
+                       }else if (controll.semaphoreSubir.getQueueLength() >= MAX_ESTUDIANTES || controll.semaphoreBajar.getQueueLength() == 0){
+                           byte bColaSubida = (byte) controll.semaphoreSubir.getQueueLength();
+                           if (controll.semaphoreSubir.getQueueLength() > MAX_ESTUDIANTES){
+                               bColaSubida = MAX_ESTUDIANTES;
+                           }
+                           System.out.println("Han subido: " + bColaSubida + " estudiantes.");
+                            controll.semaphoreSubir.release();
+                       }
+                   }
+               } catch (InterruptedException e) {
+                   e.printStackTrace();
+               }
+           }while (true);
         }
     }
 
     public class Alumno implements Runnable {
-        private byte id;
+        private int id;
         byte bByte;
 
         public Alumno(byte id) {
             this.id = id;
         }
 
-        public byte getId() {
+        public int getId() {
             return id;
         }
 
-        public void setId(byte id) {
+        public void setId(int id) {
             this.id = id;
         }
 
@@ -161,57 +108,39 @@ public class Principal {
             this.bByte = bByte;
         }
 
-        public InterruptedException getE() {
-            return e;
-        }
-
-        public void setE(InterruptedException e) {
-            this.e = e;
-        }
-
         @Override
         public void run() {
-
             setbByte((byte) (Math.random() * 2));
-
+            try {
             if(bByte == SUBIR){
                 System.out.println("El alumno: " + getId() + " va a subir.");
                 controll.colaSubir.add(this);
-                try {
+
                     controll.semaphoreSubir.acquire();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+
             }else{
                 System.out.println("El alumno: " + getId() + " va a bajar.");
+                controll.colaBajar.add(this);
+                controll.semaphoreBajar.acquire();
             }
-
-
-
-
-
-
-
-
-
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     private void executeMultiThreading() throws InterruptedException {
+        int iContador = 0;
 
+        new Thread(new Escalera()).start();
         while (true) {
-            int iCont = 0;
-            Thread.sleep(200);
-            new Thread(new AlumnoSubir((byte) iCont)).start();
-            iCont++;
-            Thread.sleep(200);
-            new Thread(new AlumnoBajar((byte) iCont)).start();
-            iCont ++;
+            new Thread(new Alumno((byte) iContador)).start();
+            Thread.sleep(1000);
+            iContador++;
         }
     }
 
     public static void main(String[] args) {
-
         try {
             Principal principal = new Principal();
             principal.executeMultiThreading();
